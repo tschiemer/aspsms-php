aaf653a09b1e9b3f1b32e1ca0ade14e55d896e55
tschiemer <me@filou.se>
2014-02-12 14:06:28 +0100

    added webservice status descriptions

--- lib/Aspsms/Helpers.php
+++ lib/Aspsms/Helpers.php
@@ -3,10 +3,71 @@
 namespace Aspsms;
 
 /**
+ * Webservice statuscode strings
+ * 
+ * @version 1
+ * @package aspsms
+ * @license LGPL v3 http://www.gnu.org/licenses/lgpl-3.0.txt
+ * @copyright 2013 Philip Tschiemer, <tschiemer@filou.se>
+ * @link https://github.com/tschiemer/aspsms-php
+ * 
+ * @param int|string $code
+ * @return string|null
+ */
+if ( ! function_exists('statusDescriptionString'))
+{
+    function statusDescriptionString($code)
+    {
+        switch(intval($code))
+        {
+            case 1: return 'OK';
+            case 2: return 'Connect failed.';
+            case 3: return 'Authorization failed.';
+            case 4: return 'Binary file not found. Please check the location.';
+            case 5: return 'Not enough credits available. Please recharge your account to proceed.';
+            case 6: return 'Time out error.';
+            case 7: return 'Transmission error. Please try it again.';
+            case 8: return 'Invalid UserKey. Please check the spelling of the UserKey.';
+            case 9: return 'Invalid Password.';
+            case 10: return 'Invalid originator. A maximum of 11 characters is allowed for alphanumeric originators.';
+            case 11: return 'Invalid message date. Please verify the data.';
+            case 12: return 'Invalid binary data. Please verify the data.';
+            case 13: return 'Invalid binary file. Please check the file type.';
+            case 14: return 'Invalid MCC. Please check the number.';
+            case 15: return 'Invalid MNC. Please check the number.';
+            case 16: return 'Invalid XSer.';
+            case 17: return 'Invalid URL buffered message notification string.';
+            case 18: return 'Invalid URL delivery notification string.';
+            case 19: return 'Invalid URL non delivery notification string.';
+            case 20: return 'Missing a recipient. Please specify at least one recipient.';
+            case 21: return 'Missing binary data. Please specify some data.';
+            case 22: return 'Invalid deferred delivery time. Please check the format.';
+            case 23: return 'Missing transaction reference number.';
+            case 24: return 'Service temporarely not available.';
+            case 25: return 'User access denied.';
+            case 28: return 'No Originator Restrictions.';
+            case 29: return 'Originator Authorization Pending.';
+            case 30: return 'Originator Not Authorized.';
+            case 31: return 'Originator already authorized';
+            case 32: return 'No Notification Recipient Restrictions.';
+            case 33: return 'Notification Recipient Authorization Pending.';
+            case 34: return 'Notification Recipient Not Authorized.';
+            case 35: return 'Notification Recipient Already Authorized.';
+            case 36: return 'Security Token authorization pending';
+            case 37: return 'Security Token not authorized';
+            case 38: return 'Security Token not found';
+            case 39: return 'Security Token already authorized';
+                
+            default: return NULL;
+        }
+    }
+}
+
+/**
  * Code to description map of delivery statuses
  * 
  * @version 1
- * @package aspsms.adapter.codeigniter
+ * @package aspsms
  * @license LGPL v3 http://www.gnu.org/licenses/lgpl-3.0.txt 
  * @copyright 2013 Philip Tschiemer, <tschiemer@filou.se>
  * @link https://github.com/tschiemer/aspsms-php
@@ -34,7 +95,7 @@ if ( ! function_exists('deliveryStatusString'))
  * Code to description map of tracking reason codes
  * 
  * @version 1
- * @package aspsms.adapter.codeigniter
+ * @package aspsms
  * @license LGPL v3 http://www.gnu.org/licenses/lgpl-3.0.txt 
  * @copyright 2013 Philip Tschiemer, <tschiemer@filou.se>
  * @link https://github.com/tschiemer/aspsms-php
