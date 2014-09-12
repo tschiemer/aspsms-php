0f659695606e7e9b2dd2c3c1b7083be32ceb4fe7
Filou <filou@filouse.local>
2014-09-12 19:54:58 +0200

    phpdoc

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Aspsms"],["c","Aspsms_demo"],["c","DateTime"],["c","DateTimeZone"],["c","DOMDocument"],["c","DOMNode"],["c","Exception"],["c","SoapClient"],["c","SoapFault"],["c","tschiemer\\Aspsms\\AbstractClient"],["c","tschiemer\\Aspsms\\AbstractSimpleClient"],["c","tschiemer\\Aspsms\\Http\\HttpClient"],["c","tschiemer\\Aspsms\\Http\\HttpSimpleClient"],["c","tschiemer\\Aspsms\\Request"],["c","tschiemer\\Aspsms\\Response"],["c","tschiemer\\Aspsms\\ServiceException"],["c","tschiemer\\Aspsms\\SimpleClient"],["c","tschiemer\\Aspsms\\Soap\\SoapClient"],["c","tschiemer\\Aspsms\\Soap\\SoapSimpleClient"],["c","tschiemer\\Aspsms\\Strings"],["c","tschiemer\\Aspsms\\Xml\\XmlClient"]];
